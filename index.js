12113593492e01f14ee362cb1e1f0a0468fa6b08
Deborah Wade <sopranosamurai@yahoo.com>
2023-01-09 07:37:29 -0800

    started index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// imports
+import inquirer from 'inquirer';
+
+import Manager from './lib/Manager';
+import Engineer from './lib/Engineer';
+import Intern from './lib/Intern';
+
+import generateHTML from 
